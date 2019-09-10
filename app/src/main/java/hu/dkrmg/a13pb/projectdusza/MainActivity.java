dbc11674c47af6c78baa8f5b7dfb0bf44208785a
Posfay Benedek <benedek@posfay.com>
2019-09-10 22:04:36 +0200

    Init

--- app/src/main/java/hu/dkrmg/a13pb/projectdusza/MainActivity.java
+++ app/src/main/java/hu/dkrmg/a13pb/projectdusza/MainActivity.java
@@ -1,15 +1,13 @@
 package hu.dkrmg.a13pb.projectdusza;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
 }
