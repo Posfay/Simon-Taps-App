1e0935895c5af6506d656ea5289ca2c74c6d8129
Posfay Benedek <benedek@posfay.com>
2019-10-09 23:21:57 +0200

    okhttp mainActivitybe

--- app/src/main/java/hu/dkrmg/a13pb/projectdusza/MainActivity.java
+++ app/src/main/java/hu/dkrmg/a13pb/projectdusza/MainActivity.java
@@ -1,34 +1,89 @@
 package hu.dkrmg.a13pb.projectdusza;
 
+import java.io.IOException;
+
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.json.JSONObject;
-import org.w3c.dom.Text;
-
-import java.io.IOException;
-import java.io.UncheckedIOException;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class MainActivity extends Activity {
 
-  TextView text;
+  public TextView text;
+
+  private Handler handler;
+
+  public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
     text = findViewById(R.id.textout);
+
+    handler = new Handler(Looper.getMainLooper());
   }
+
   public void requestClick(View v) {
+
     JSONObject json = new JSONObject();
-    try {
-      text.setText(OkHttpHandler.post("http://example.com",json));
-    } catch (IOException e) {
-      Toast.makeText(this, "Hiba a kérésben", Toast.LENGTH_SHORT).show();
-    }
+    String url = "http://example.com";
+
+    postRequest(url, json);
+  }
+
+  public void postRequest(String url, JSONObject json) {
+
+    OkHttpClient client = new OkHttpClient();
+    Request request = createRequest(url, json);
+
+    client.newCall(request).enqueue(new Callback() {
+      @Override
+      public void onFailure(Call call, IOException e) {
+        String mMessage = e.getMessage();
+        Log.w("failure Response", mMessage);
+        Toast.makeText(MainActivity.this, "Hiba a keresben!", Toast.LENGTH_SHORT).show();
+      }
+
+      @Override
+      public void onResponse(Call call, final Response response) {
+        handler.post(new Runnable() {
+          @Override
+          public void run() {
+            try {
+              text.setText(response.body().string());
+            } catch (IOException e) {
+              Toast.makeText(MainActivity.this, "Hiba a keresben!", Toast.LENGTH_SHORT).show();
+            }
+          }
+        });
+      }
+    });
+
+  }
+
+  private Request createRequest(String url, JSONObject json) {
+    RequestBody body = RequestBody.create(JSON, json.toString());
+    return new Request.Builder()
+        .url(url)
+        .post(body)
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .build();
   }
 }
