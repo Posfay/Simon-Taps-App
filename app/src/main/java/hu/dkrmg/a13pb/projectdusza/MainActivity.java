6e37290ada816e079792a5167c49534e943af68f
csumpasd <csutoramarton@gmail.com>
2019-10-09 21:15:17 +0200

    button, textin, textout

--- app/src/main/java/hu/dkrmg/a13pb/projectdusza/MainActivity.java
+++ app/src/main/java/hu/dkrmg/a13pb/projectdusza/MainActivity.java
@@ -2,12 +2,25 @@ package hu.dkrmg.a13pb.projectdusza;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends Activity {
+
+  TextView szoveg;
+  EditText bemenet;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    szoveg = findViewById(R.id.textout);
+    bemenet = findViewById(R.id.textin);
+  }
+  public void requestClick(View v) {
+    szoveg.setText(bemenet.getText());
   }
 }
