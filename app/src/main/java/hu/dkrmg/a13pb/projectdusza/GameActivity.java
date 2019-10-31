c5ab166d9716ae459838a1c10721f3ba251992a1
kovacsand <k.andras9@gmail.com>
2019-10-31 16:38:41 +0100

    Működik a showing_pattern is, tesztelve mobilon.

--- app/src/main/java/hu/dkrmg/a13pb/projectdusza/GameActivity.java
+++ app/src/main/java/hu/dkrmg/a13pb/projectdusza/GameActivity.java
@@ -18,14 +18,9 @@ import java.util.List;
 
 public class GameActivity extends Activity implements AsyncResponse {
 
-    public static final long DELAY_MILLIS = 500;
-    public static final long ROUNDS = 2;
-
     public OkHttpHandler okHttpHandler;
     public String playerId;
     public String roomId;
-
-
 
     Button greenButton;
     Button redButton;
@@ -33,66 +28,22 @@ public class GameActivity extends Activity implements AsyncResponse {
     Button blueButton;
     Button yourButton;
 
+    long numOfPlayers = -1;
     Long tileId = null;
     List<Integer> pattern;
     String wordPattern = "";
-    Handler timerHandler = new Handler();
+    Boolean shown = false;
+
     Handler getStateTimerHandler = new Handler();
+    long intervalMilli = 1000;
+
+    Handler timerHandler = new Handler();
     Handler delayHandler = new Handler();
-    Integer counter = 0;
-
-    Runnable timerRunnable = new Runnable() {
-
-        @Override
-        public void run() {
-            Integer current = pattern.remove(0);
-            counter++;
-            if (current == 1) {
-                greenButton.setBackgroundResource(R.drawable.button_green_active);
-                delayHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        greenButton.setBackgroundResource(R.drawable.button_green);
-                    }
-                }, DELAY_MILLIS);
-            }
-            if (current == 2) {
-                redButton.setBackgroundResource(R.drawable.button_red_active);
-                delayHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        redButton.setBackgroundResource(R.drawable.button_red);
-                    }
-                }, DELAY_MILLIS);
-            }
-            if (current == 3) {
-                yellowButton.setBackgroundResource(R.drawable.button_yellow_active);
-                delayHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        yellowButton.setBackgroundResource(R.drawable.button_yellow);
-                    }
-                }, DELAY_MILLIS);
-            }
-            if (current == 4) {
-                blueButton.setBackgroundResource(R.drawable.button_blue_active);
-                delayHandler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        blueButton.setBackgroundResource(R.drawable.button_blue);
-                    }
-                }, DELAY_MILLIS);
-            }
-            if (counter == ROUNDS*4) {
-                return;
-            }
-            timerHandler.postDelayed(this, DELAY_MILLIS+100);
-        }
-
-    };
+    public static final long DELAY_MILLIS = 500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
@@ -105,36 +56,23 @@ public class GameActivity extends Activity implements AsyncResponse {
         playerId = getIntent().getStringExtra("EXTRA_PLAYER_ID");
         roomId = getIntent().getStringExtra("EXTRA_ROOM_ID");
 
-        pattern = new ArrayList<Integer>();
-        for (int n = 1; n <= ROUNDS; n++) {
-            for (int i = 1; i <= 4; i++) {
-                pattern.add(i);
-            }
-        }
-        Collections.shuffle(pattern);
+        pattern = new ArrayList<>();
 
-        for (Integer current : pattern) {
-            wordPattern += current;
-        }
-        Log.i("minta", wordPattern);
-
-
-        Log.i("minta2", pattern.toString());
+        getStateTimerHandler.postDelayed(getStateTimerRunnable, 0);
     }
 
     Runnable getStateTimerRunnable = new Runnable() {
         @Override
         public void run() {
 
-            String url = "https://szerver3.dkrmg.sulinet.hu:8080/simon-taps/state?room_id="+roomId+"&player_id="+playerId;
+            String url = "http://szerver3.dkrmg.sulinet.hu:8080/simon-taps/state?room_id="+roomId+"&player_id="+playerId;
+
             okHttpHandler = new OkHttpHandler(GameActivity.this);
             okHttpHandler.getRequest(url);
 
-
-            getStateTimerHandler.postDelayed(this, 300);
+            getStateTimerHandler.postDelayed(this, intervalMilli);
         }
     };
-
 
     @Override
     public void onRequestComplete(String responseJsonString) {
@@ -142,67 +80,136 @@ public class GameActivity extends Activity implements AsyncResponse {
         JSONObject payloadJson = null;
         String status = null;
         String state = null;
-        long num = -1;
+
 
         try {
+
             payloadJson = new JSONObject(responseJsonString);
-            status = payloadJson.getString("status");
-            state = payloadJson.getString("game_state");
+            status = payloadJson.optString("status");
+            state = payloadJson.optString("game_state");
 
         } catch (JSONException e) {
             e.printStackTrace();
         }
 
-        if (status.equals("OK")) {
-
-            //WAITING
-            if (state == "waiting") {
-                num = payloadJson.optLong("number_of_players");
-                yourButton.setText(num+"");
-            }
-            //PREPARING
-            if (state == "preparing") {
-                tileId = payloadJson.optLong("tile_id");
-                if (tileId == 1) {
-                    yourButton.setBackgroundColor(getResources().getColor(R.color.green));
-                }
-                if (tileId == 2) {
-                    yourButton.setBackgroundColor(getResources().getColor(R.color.red));
-                }
-                if (tileId == 3) {
-                    yourButton.setBackgroundColor(getResources().getColor(R.color.yellow));
-                }
-                if (tileId == 4) {
-                    yourButton.setBackgroundColor(getResources().getColor(R.color.blue));
-                }
-            }
-            //SHOWING PATTERN
-            if (state == "showing_pattern") {
-                wordPattern = payloadJson.optString("pattern");
-                pattern.clear();
-                for (int i=0; i<wordPattern.length(); i++) {
-                    pattern.add(Integer.valueOf(String.valueOf(wordPattern.charAt(i))));
-                }
-                timerHandler.postDelayed(timerRunnable, 1000);
-            }
-
-
-        }
-        else {
+        if (!status.equals("OK")) {
             return;
         }
 
+        //WAITING
+        if (state.equals(StateUtils.WAITING))  {
+
+            numOfPlayers = payloadJson.optLong("number_of_players");
+            yourButton.setText(numOfPlayers+"");
+        }
+        //PREPARING
+        if (state.equals(StateUtils.PREPARING)) {
+
+            intervalMilli = 200;
+            tileId = payloadJson.optLong("tile_id");
+
+            if (tileId == 1) {
+                yourButton.setBackgroundColor(getResources().getColor(R.color.green));
+            }
+            if (tileId == 2) {
+                yourButton.setBackgroundColor(getResources().getColor(R.color.red));
+            }
+            if (tileId == 3) {
+                yourButton.setBackgroundColor(getResources().getColor(R.color.yellow));
+            }
+            if (tileId == 4) {
+                yourButton.setBackgroundColor(getResources().getColor(R.color.blue));
+            }
+        }
+        //SHOWING PATTERN
+        if (state.equals(StateUtils.SHOWING_PATTERN)) {
+
+            if (shown) {
+                return;
+            }
+
+            wordPattern = payloadJson.optString("pattern");
+            pattern.clear();
+
+
+            for (int i=0; i<wordPattern.length(); i++) {
+                pattern.add(Integer.valueOf(String.valueOf(wordPattern.charAt(i))));
+            }
+
+            shown = true;
+
+            Log.i("minta", pattern.toString());
+            displayPattern();
+
+        }
+
+        getStateTimerHandler.postDelayed(getStateTimerRunnable, intervalMilli);
+
     }
 
+
+    public void displayPattern() {
+        Runnable timerRunnable = new Runnable() {
+
+            Integer counter = pattern.size();
+
+            @Override
+            public void run() {
+                Integer current = pattern.remove(0);
+                counter --;
+
+                if (current == 1) {
+
+                    greenButton.setBackgroundResource(R.drawable.button_green_active);
+                    delayHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            greenButton.setBackgroundResource(R.drawable.button_green);
+                        }
+                    }, DELAY_MILLIS);
+                }
+                if (current == 2) {
+
+                    redButton.setBackgroundResource(R.drawable.button_red_active);
+                    delayHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            redButton.setBackgroundResource(R.drawable.button_red);
+                        }
+                    }, DELAY_MILLIS);
+                }
+                if (current == 3) {
+
+                    yellowButton.setBackgroundResource(R.drawable.button_yellow_active);
+                    delayHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            yellowButton.setBackgroundResource(R.drawable.button_yellow);
+                        }
+                    }, DELAY_MILLIS);
+                }
+                if (current == 4) {
+
+                    blueButton.setBackgroundResource(R.drawable.button_blue_active);
+                    delayHandler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            blueButton.setBackgroundResource(R.drawable.button_blue);
+                        }
+                    }, DELAY_MILLIS);
+                }
+                if (counter <= 0) {
+                    return;
+                }
+                timerHandler.postDelayed(this, DELAY_MILLIS+100);
+            }
+        };
+
+        timerHandler.postDelayed(timerRunnable, 0);
+
+    }
 
     public void youOnClick(View v) {
 
-
-
-
-
-
-        getStateTimerHandler.postDelayed(getStateTimerRunnable, 1000);
     }
-
 }
---
+++ app/src/main/java/hu/dkrmg/a13pb/projectdusza/StateUtils.java
@@ -0,0 +1,16 @@
+package hu.dkrmg.a13pb.projectdusza;
+
+public class StateUtils {
+
+    public static final String WAITING = "waiting";
+    public static final String PREPARING = "preparing";
+    public static final String SHOWING_PATTERN = "showing_pattern";
+    public static final String PLAYING = "playing";
+    public static final String SUCCESSFUL_END = "successful_end";
+    public static final String FAIL_END = "fail_end";
+
+    private StateUtils() {
+
+    }
+
+}
