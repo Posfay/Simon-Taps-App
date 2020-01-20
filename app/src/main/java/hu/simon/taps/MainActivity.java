ee0d555bf5e304663a1e1f8a79d70fbc5962634e
csumpasd <csutoramarton@gmail.com>
2020-01-20 21:52:35 +0100

    1.1.5 fullscreen in working state

--- app/src/main/java/hu/simon/taps/MainActivity.java
+++ app/src/main/java/hu/simon/taps/MainActivity.java
@@ -66,9 +66,6 @@ public class MainActivity extends Activity implements AsyncResponse {
 
     vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
 
-    getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-        WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
-
     // Always uppercase in textbox
     roomIdEditText.addTextChangedListener(new TextWatcher() {
 
@@ -160,8 +157,24 @@ public class MainActivity extends Activity implements AsyncResponse {
         // "#ffff00" alt. yellow
         break;
     }
+    //fullscreen
   }
-
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
   // Checking internet connection
   public void connectionCheck() {
 
